5dae4306706c229cbcf041eb7c64574d4fe2300c
pavlej <pavlej@NSDT-PAVLEJ.ccbill-hq.local>
2020-02-12 16:38:37 +0100

    dev properties added

---
+++ config/properties_dev.go
@@ -0,0 +1,9 @@
+// +build dev
+package config
+
+const (
+	// Hostname represents the URL entrypoint of our application
+	Hostname = "https://api-dev.phoenixnap.com/bmc/v0/"
+	// TokenURL represents the URL of the OpenID Connect provider from where we can retrieve a token
+	TokenURL = "https://auth-dev.phoenixnap.com/auth/realms/BMC/protocol/openid-connect/token"
+)
