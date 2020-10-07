e59610e1dbcc5c70d1e692489286559ec778462f
pavlej <pavlej@NSDT-PAVLEJ.ccbill-hq.local>
2020-10-08 00:55:45 +0200

    dev api v1

--- config/properties.go
+++ config/properties.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	// Hostname represents the URL entrypoint of our application
-	Hostname = "https://api.phoenixnap.com/bmc/v1beta/"
+	Hostname = "https://api-dev.phoenixnap.com/bmc/v1/"
 	// TokenURL represents the URL of the OpenID Connect provider from where we can retrieve a token
-	TokenURL = "https://auth.phoenixnap.com/auth/realms/BMC/protocol/openid-connect/token"
+	TokenURL = "https://auth-dev.phoenixnap.com/auth/realms/BMC/protocol/openid-connect/token"
 )
