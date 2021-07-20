50039bb1dbfe6f69f357d1e7b294d9bc46e876c9
duskol <duskol@phoenixnap.com>
2021-07-20 20:57:58 +0200

    get sshkeys command impl

---
+++ command/getsshkeys.go
@@ -0,0 +1,28 @@
+package command
+
+import (
+	"net/http"
+
+	"github.com/PNAP/bmc-api-sdk/client"
+)
+
+// GetSshKeysCommand represents command for sshkeys retrieval
+type GetSshKeysCommand struct {
+	requester client.Requester
+}
+
+// Execute retrieves all ssh-keys
+func (command *GetSshKeysCommand) Execute() (*http.Response, error) {
+	var req = command.requester
+	return req.Get("ssh-keys")
+}
+
+// SetRequester sets requester to the command
+func (command *GetSshKeysCommand) SetRequester(requester client.Requester) {
+	command.requester = requester
+}
+
+//NewGetSshKeysCommand constructs new commmand of this type
+func NewGetSshKeysCommand(requester client.Requester) *GetSshKeysCommand {
+	return &GetSshKeysCommand{requester}
+}
