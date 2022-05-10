174e0da80dc0bba170b1376e247b5b95e8aef054
Gabriel Camilleri <gabrielc@ccbill.com>
2022-05-10 10:25:08 +0200

    test_utils implementation

---
+++ tests/test_utils.go
@@ -0,0 +1,154 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func (t TestUtilsImpl) setup_expectation(requestToMock *string, responseToGet *string) *http.Response {
+
+	type Times struct {
+		remainingTimes int
+		unlimited      bool
+	}
+
+	type Body struct {
+		httpRequest  string
+		httpResponse string
+		times        Times
+	}
+
+	url := "http://localhost:1080/expectation"
+
+	body := Body{
+		httpRequest:  *requestToMock,
+		httpResponse: *responseToGet,
+		times: Times{
+			remainingTimes: 1,
+			unlimited:      false,
+		},
+	}
+
+	client := &http.Client{}
+
+	json, err := json.Marshal(body)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(json))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+
+}
+
+func (t TestUtilsImpl) verify_expectation_matched_times(expectationId string, times int) *http.Response {
+
+	url := "http://localhost:1080/verify"
+
+	type ExpectationId struct {
+		id string
+	}
+
+	type Times struct {
+		atLeast int
+		atMost  int
+	}
+
+	type Body struct {
+		expectationId ExpectationId
+		times         Times
+	}
+
+	body := Body{
+		expectationId: ExpectationId{
+			id: "",
+		},
+		times: Times{
+			atLeast: times,
+			atMost:  times,
+		},
+	}
+
+	client := &http.Client{}
+
+	json, err := json.Marshal(body)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(json))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+}
+
+func reset_expectations() {
+	url := "http://localhost:1080/reset"
+
+	http.NewRequest(http.MethodPut, url, http.NoBody)
+
+}
+
+func generate_payloads_from(filename string, payloadsPath string) (request Request, response Response) {
+
+	if payloadsPath == "" {
+		payloadsPath = "./payloads"
+	}
+
+	var payload Payload
+
+	file, err := ioutil.ReadFile("#{payloadsPath}/#{filename}.json")
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(file, &payload)
+
+	return payload.request, payload.response
+
+}
+
+func generate_query_params(request Request) Opts {
+
+	opts := Opts{}
+
+	opts.Opt = make(map[string]string)
+
+	qplist := request.queryStringParameters
+	for _, qp := range qplist {
+		values := qp.values[0]
+		opts.Opt[qp.name] = values
+	}
+
+	return opts
+}
+
+func extract_id_from(request *Request, symbol *string) (id string) {
+	return request.pathParameters.id[0]
+}
+
+func extract_request_body(request *Request) (json Json) {
+	return request.body.json
+}
