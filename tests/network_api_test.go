563cddf1a901594fe8d631fe479ecff71d1a4e95
Gabriel Camilleri <gabrielc@ccbill.com>
2022-05-25 10:08:56 +0200

    implementing get and create private net tests

--- tests/network_api_test.go
+++ tests/network_api_test.go
@@ -12,9 +12,9 @@ import (
 
 type NetworkApiTestSuite struct {
 	suite.Suite
-	ctx context.Context
+	ctx           context.Context
 	configuration *networkapi.Configuration
-	apiClient *networkapi.APIClient
+	apiClient     *networkapi.APIClient
 }
 
 // this function executes before each test
@@ -46,52 +46,94 @@ func (suite *NetworkApiTestSuite) verifyCalledOnce(expectationId string) {
 }
 
 func (suite *NetworkApiTestSuite) TestGetPrivateNetworks() {
+	// Generate payload
+	request, response := TestUtilsImpl{}.generatePayloadsFrom("networkapi/networks_get", "./payloads")
 
+	// Extract the response expectation id
+	expectationId := TestUtilsImpl{}.setupExpectation(request, response, 1)
+
+	// Fetch a map of query parameters
+	qpMap := TestUtilsImpl{}.generateQueryParams(request)
+
+	loc := fmt.Sprintf("%v", qpMap["location"])
+
+	// Operation Execution
+	result, _, _ := suite.apiClient.PrivateNetworksApi.PrivateNetworksGet(suite.ctx).Location(loc).Execute()
+
+	// Convert the result and response body to json strings
+	jsonResult, _ := json.Marshal(result)
+	jsonResponseBody, _ := json.Marshal(response.Body)
+
+	// Asserts
+	suite.Equal(jsonResult, jsonResponseBody)
+
+	// Verify
+	suite.verifyCalledOnce(expectationId)
 }
 
 func (suite *NetworkApiTestSuite) TestCreatePrivateNetwork() {
+	// Generate payload
+	request, response := TestUtilsImpl{}.generatePayloadsFrom("networkapi/networks_post", "./payloads")
 
+	// Extract the response expectation id
+	expectationId := TestUtilsImpl{}.setupExpectation(request, response, 1)
+
+	byteData := TestUtilsImpl{}.extractRequestBody(request)
+	var networkCreate networkapi.PrivateNetworkCreate
+	json.Unmarshal(byteData, &networkCreate)
+
+	// Operation Execution
+	result, _, _ := suite.apiClient.PrivateNetworksApi.PrivateNetworksPost(suite.ctx).PrivateNetworkCreate(networkCreate).Execute()
+
+	// Convert the result and response body to json strings
+	jsonResult, _ := json.Marshal(result)
+	jsonResponseBody, _ := json.Marshal(response.Body)
+
+	suite.Equal(jsonResult, jsonResponseBody)
+
+	// Verify
+	suite.verifyCalledOnce(expectationId)
 }
 
-func (suite *NetworkApiTestSuite) TestGetPrivateNetworkById() {
+// func (suite *NetworkApiTestSuite) TestGetPrivateNetworkById() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestPutPrivateNetworkById() {
+// func (suite *NetworkApiTestSuite) TestPutPrivateNetworkById() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestDeletePrivateNetworkById() {
+// func (suite *NetworkApiTestSuite) TestDeletePrivateNetworkById() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestGetPublicNetworks() {
+// func (suite *NetworkApiTestSuite) TestGetPublicNetworks() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestCreatePublicNetwork() {
+// func (suite *NetworkApiTestSuite) TestCreatePublicNetwork() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestGetPublicNetworkById() {
+// func (suite *NetworkApiTestSuite) TestGetPublicNetworkById() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestPatchPublicNetworkById() {
+// func (suite *NetworkApiTestSuite) TestPatchPublicNetworkById() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestDeletePublicNetworkById() {
+// func (suite *NetworkApiTestSuite) TestDeletePublicNetworkById() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestPostPublicNetworkIpBlockByPublicNetworkId() {
+// func (suite *NetworkApiTestSuite) TestPostPublicNetworkIpBlockByPublicNetworkId() {
 
-}
+// }
 
-func (suite *NetworkApiTestSuite) TestDeletePublicNetworkIpBlocksByPublicNetworkIdAndIpBlockID() {
+// func (suite *NetworkApiTestSuite) TestDeletePublicNetworkIpBlocksByPublicNetworkIdAndIpBlockID() {
 
-}
+// }
 
 func TestNetworkApiTestSuite(t *testing.T) {
 	suite.Run(t, new(NetworkApiTestSuite))
