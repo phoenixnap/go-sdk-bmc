5f51b7c2c81925f37c7cb59e79e7384cf01cb2a5
andrej <andrej@ccbill.com>
2022-06-09 10:25:07 +0200

    Removed snapshot in version

--- auditapi/VERSION.go
+++ auditapi/VERSION.go
@@ -1,3 +1,3 @@
 package auditapi
 
-const SdkVersion = "1.0.4-SNAPSHOT"
+const SdkVersion = "1.0.4"
--- billingapi/VERSION.go
+++ billingapi/VERSION.go
@@ -1,3 +1,3 @@
 package billingapi
 
-const SdkVersion = "1.0.0-SNAPSHOT"
+const SdkVersion = "1.0.0"
