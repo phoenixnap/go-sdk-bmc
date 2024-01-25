bffe6e6f64c922db53f3349eeeb1e394f8005d7b
ajenkins <andrej@ccbill.com>
2024-01-25 09:01:51 +0100

    Added invoice tests

--- tests/invoicing_api_test.go
+++ tests/invoicing_api_test.go
@@ -2,7 +2,12 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/phoenixnap/go-sdk-bmc/invoicingapi"
 	"github.com/stretchr/testify/suite"
@@ -48,33 +53,127 @@ func (suite *InvoicingApiTestSuite) verifyCalledOnce(expectationId string) {
 	suite.Equal(202, verifyResult.StatusCode)
 }
 
-// func (suite *InvoicingApiTestSuite) TestGetLocations() {
-// 	// generate payload
-// 	request, response := TestUtilsImpl{}.generatePayloadsFrom("invoicingapi/locations_get", "./payloads")
+func (suite *InvoicingApiTestSuite) TestGetInvoices() {
+	// generate payload
+	request, response := TestUtilsImpl{}.generatePayloadsFrom("invoicingapi/invoices_get", "./payloads")
 
-// 	// extract expectation id
-// 	expectationId := TestUtilsImpl{}.setupExpectation(request, response, 1)
+	// extract expectation id
+	expectationId := TestUtilsImpl{}.setupExpectation(request, response, 1)
 
-// 	// fetch query parameters
-// 	qpMap := TestUtilsImpl{}.generateQueryParams(request)
+	// fetch query parameters
+	qpMap := TestUtilsImpl{}.generateQueryParams(request)
 
-// 	location := fmt.Sprintf("%v", qpMap["location"])
-// 	productCategory := fmt.Sprintf("%v", qpMap["productCategory"])
+	// extracting into variables
+	number := fmt.Sprintf("%v", qpMap["number"])
+	status := fmt.Sprintf("%v", qpMap["status"])
+	sentOnFrom, _ := time.Parse(time.RFC3339, fmt.Sprintf("%v", qpMap["sentOnFrom"]))
+	sentOnTo, _ := time.Parse(time.RFC3339, fmt.Sprintf("%v", qpMap["sentOnTo"]))
+	limit, _ := strconv.ParseInt(fmt.Sprintf("%v", qpMap["limit"]), 10, 32)
+	offset, _ := strconv.ParseInt(fmt.Sprintf("%v", qpMap["offset"]), 10, 32)
+	sortField := fmt.Sprintf("%v", qpMap["sortField"])
+	sortDirection := fmt.Sprintf("%v", qpMap["sortDirection"])
 
-// 	// execution
-// 	result, _, _ := suite.apiClient.LocationsAPI.
-// 		GetLocations(suite.ctx).
-// 		Location(invoicingapi.LocationEnum(location)).
-// 		ProductCategory(invoicingapi.ProductCategoryEnum(productCategory)).
-// 		Execute()
+	// execution
+	result, r, err := suite.apiClient.InvoicesAPI.InvoicesGet(context.Background()).Number(number).Status(status).SentOnFrom(sentOnFrom).SentOnTo(sentOnTo).Limit(int32(limit)).Offset(int32(offset)).SortField(sortField).SortDirection(sortDirection).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling 'InvoicesAPI.InvoicesGet': %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
 
-// 	// convert to json strings
-// 	jsonResult, _ := json.Marshal(result)
-// 	jsonResponseBody, _ := json.Marshal(response.Body)
+	// convert the result and response body to json strings
+	jsonResult, _ := json.Marshal(result)
+	jsonResponseBody, _ := json.Marshal(response.Body)
 
-// 	// asserts
-// 	suite.Equal(jsonResult, jsonResponseBody)
+	// asserts
+	suite.Equal(string(jsonResult), string(jsonResponseBody))
 
-// 	// verify
-// 	suite.verifyCalledOnce(expectationId)
-// }
+	// verify
+	suite.verifyCalledOnce(expectationId)
+}
+
+func (suite *InvoicingApiTestSuite) TestGetInvoiceById() {
+	// generate payload
+	request, response := TestUtilsImpl{}.generatePayloadsFrom("invoicingapi/invoices_get_by_id", "./payloads")
+
+	// extract expectation id
+	expectationId := TestUtilsImpl{}.setupExpectation(request, response, 1)
+
+	// extracting path parameter
+	invoiceId := request.PathParameters["id"][0]
+
+	// execution
+	result, r, err := suite.apiClient.InvoicesAPI.InvoicesInvoiceIdGet(context.Background(), invoiceId).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling 'InvoicesAPI.InvoicesInvoiceIdGet': %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	// convert the result and response body to json strings
+	jsonResult, _ := json.Marshal(result)
+	jsonResponseBody, _ := json.Marshal(response.Body)
+
+	// asserts
+	suite.Equal(string(jsonResult), string(jsonResponseBody))
+
+	// verify
+	suite.verifyCalledOnce(expectationId)
+}
+
+func (suite *InvoicingApiTestSuite) TestInvoicePay() {
+	// generate payload
+	request, response := TestUtilsImpl{}.generatePayloadsFrom("invoicingapi/invoices_pay_post", "./payloads")
+
+	// extract expectation id
+	expectationId := TestUtilsImpl{}.setupExpectation(request, response, 1)
+
+	// extracting path parameter
+	invoiceId := request.PathParameters["id"][0]
+
+	// extracting request body
+	body, _ := json.Marshal(request.Body.Json)
+	var invoicePaymentRequest map[string]interface{}
+	json.Unmarshal(body, &invoicePaymentRequest)
+
+	// execution
+	result, r, err := suite.apiClient.InvoicesAPI.InvoicesInvoiceIdPayPost(context.Background(), invoiceId).Body(invoicePaymentRequest).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling 'InvoicesAPI.InvoicesInvoiceIdPayPost': %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	// convert the result and response body to json strings
+	jsonResult, _ := json.Marshal(result)
+	jsonResponseBody, _ := json.Marshal(response.Body)
+
+	// asserts
+	suite.Equal(string(jsonResult), string(jsonResponseBody))
+
+	// verify
+	suite.verifyCalledOnce(expectationId)
+}
+
+func (suite *InvoicingApiTestSuite) TestInvoicesGeneratePdf() {
+	// generate payload
+	request, response := TestUtilsImpl{}.generatePayloadsFrom("invoicingapi/invoices_generate_pdf_post", "./payloads")
+
+	// extract expectation id
+	expectationId := TestUtilsImpl{}.setupExpectation(request, response, 1)
+
+	// Extracting path parameter
+	invoiceId := request.PathParameters["id"][0]
+
+	// execution
+	_, r, err := suite.apiClient.InvoicesAPI.InvoicesInvoiceIdGeneratePdfPost(context.Background(), invoiceId).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling 'InvoicesAPI.InvoiceIdGeneratePdfPost': %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+
+	// we cannot assert that the response is equivalent, since this endpoint is meant to return a binary file
+	// in this case, a PDF file. to mock this, we'd likely need a binary string that represents a PDF file, and
+	// compare the response to a manually-constructed file that represents said binary string.
+	// TODO: discuss whether we want to do this?
+
+	// verify
+	suite.verifyCalledOnce(expectationId)
+}
