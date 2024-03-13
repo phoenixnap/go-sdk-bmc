ea9213fe4c9558235ef8ffa21c4f7ea82e950c4f
darrencilia <darrenc@ccbill.com>
2024-03-13 11:46:00 +0100

    Updated new SNAPSHOT versions for develop

--- billingapi/VERSION.go
+++ billingapi/VERSION.go
@@ -1,3 +1,3 @@
 package billingapi
 
-const SdkVersion = "3.0.0"
+const SdkVersion = "3.0.1-SNAPSHOT"
--- invoicingapi/VERSION.go
+++ invoicingapi/VERSION.go
@@ -1,3 +1,3 @@
 package invoicingapi
 
-const SdkVersion = "1.0.2"
+const SdkVersion = "1.0.3-SNAPSHOT"
